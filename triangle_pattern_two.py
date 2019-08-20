f45c83590fc76f907808c0650565e966aed776d1
Arvind Sharma <avin82.sharma@gmail.com>
2019-08-20 16:27:40 +0530

    Adding new file triangle_pattern_two.py

---
+++ triangle_pattern_two.py
@@ -0,0 +1,41 @@
+# PROGRAM triangle_pattern_two:
+
+'''
+Write a program to take a number input from user. Let's say the user inputs the number 4, then the program should print the following pattern (for n = 4):
+
+1
+23
+345
+4567
+'''
+
+#################################
+# Print Triangle Pattern Module #
+#################################
+
+
+def print_triangle_pattern():
+	n = int(input("Input the number of rows which is equal to the number of columns to print the desired triangle pattern: \n"))
+	i = 1
+	while i <= n:
+	# DO
+		j = 1
+		k = i
+		while j <= i:
+		# DO
+			print(k, end="")
+			j = j + 1
+			k = k + 1
+		# ENDWHILE;
+		print()
+		i = i + 1
+	# ENDWHILE;
+# END print_triangle_pattern.
+
+
+################
+# Main Program #
+################
+
+print_triangle_pattern()
+# END.
