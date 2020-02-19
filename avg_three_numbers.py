5d8d7e259cb33cc018d90c529437476db976eac8
Arvind Sharma <avin82.sharma@gmail.com>
2020-02-19 07:48:51 -0500

    Updated the file with problem statement

--- avg_three_numbers.py
+++ avg_three_numbers.py
@@ -1,5 +1,33 @@
 # PROGRAM avg_three_numbers:
 
+'''
+Find average marks
+
+Write a program to input marks of three tests of a student (all integers). Then calculate and print the average of all test marks.
+
+Input format:
+3 Test marks (in different lines)
+
+Output format:
+Average
+
+Sample Input 1:
+3
+4
+6
+
+Sample Output 1:
+4.333333333333333
+
+Sample Input 2:
+5
+10
+5
+
+Sample Output 2:
+6.666666666666667
+'''
+
 ##########################
 # Finding Average Module #
 ##########################
